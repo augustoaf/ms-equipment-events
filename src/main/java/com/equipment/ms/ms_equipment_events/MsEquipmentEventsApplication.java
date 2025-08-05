e78803a914a8e5f655a0850654131839ca111bb3
Augusto <augustoaf@gmail.com>
2025-08-05 19:08:20 -0300

    initial

---
+++ src/main/java/com/equipment/ms/ms_equipment_events/MsEquipmentEventsApplication.java
@@ -0,0 +1,13 @@
+package com.equipment.ms.ms_equipment_events;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsEquipmentEventsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsEquipmentEventsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/equipment/ms/ms_equipment_events/MsEquipmentEventsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.equipment.ms.ms_equipment_events;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsEquipmentEventsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
